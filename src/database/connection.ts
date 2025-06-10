af0dd30bdd3cfd90bb8f20433cfae738b8ad535d
dianaharoshka <dianagoroshko97@gmail.com>
2025-06-10 10:11:31 +0200

    -

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,14 +1,13 @@
-import { createPool } from 'mysql2/promise';
+import { createPool } from "mysql2/promise";
 import dotenv from "dotenv";
 dotenv.config();
- 
 
 const pool = createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+  host: process.env.HOST,
+  port: Number(process.env.PORT),
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DB,
 });
 
-export const connection = pool.getConnection()
+export const connection = pool.getConnection();
--- src/database/database.ts
+++ src/database/database.ts
@@ -2,45 +2,57 @@ import { RowDataPacket } from "mysql2";
 import { connection } from "./connection";
 
 export interface ProductInfo extends RowDataPacket {
-    id: number;
-    title:string;
-    price:number;
-    stockLevel:number;
-    color2:string;
-    description2:string;
-    categoryId:number;
-    categoryName:string;
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  stockLevel: number;
+  category_id: number;
+  is_popular: number;
+  image_url: string;
+  description2: string;
 }
 
 export interface ProductId extends RowDataPacket {
-    id: number;
+  id: number;
 }
 
+export async function getProductId(
+  name: string
+): Promise<ProductId | undefined> {
+  // SELECT id * FROM Products WHERE title=productName
+  const conn = await connection;
 
-export async function getProductId(name:string) : Promise<ProductId|undefined>{
-   // SELECT id * FROM Products WHERE title=productName
-    const conn = await connection;
-    
-    const [rows] = await conn.query<ProductId[]>("SELECT id from Products where title=?", [name])
-    if (rows.length == 0){
-        return undefined
-    }
-    return rows[0]
+  const [rows] = await conn.query<ProductId[]>(
+    "SELECT id from Products where title=?",
+    [name]
+  );
+  if (rows.length == 0) {
+    return undefined;
+  }
+  return rows[0];
 }
 
-export async function getAllProducts() : Promise<ProductInfo[]>{
-   // SELECT id * FROM Products WHERE title=productName
-    const conn = await connection;
-    
-    const [rows] = await conn.query<ProductInfo[]>("SELECT p.id,title, p.price,p.stockLevel,p.color2, p.description2, p.categoryId, c.name as categoryName from Products p JOIN Category c on c.id=p.categoryId")    
-    return rows
+export async function getAllProducts(): Promise<ProductInfo[]> {
+  // SELECT id * FROM Products WHERE title=productName
+  const conn = await connection;
+
+  const [rows] = await conn.query<ProductInfo[]>(
+    "SELECT p.id, p.title, p.price, p.stockLevel, p.description, p.category_id, p.is_popular, p.image_url, c.name AS categoryName FROM Products p JOIN Categories c ON c.id = p.category_id"
+  );
+  return rows;
 }
 
-
-export async function updateProductDescription(id:number, description:string, color:string){
-    // vi hjar ju skapat en koklumn i Products spom heter description2
-    // också en som heter color2
-    const conn = await connection;
-    await conn.execute("UPDATE Products SET description2=?, color2=? WHERE id=?",[description,color,id])
-
+export async function updateProductDescription(
+  id: number,
+  description: string,
+  color: string
+) {
+  // vi hjar ju skapat en koklumn i Products spom heter description2
+  // också en som heter color2
+  const conn = await connection;
+  await conn.execute(
+    "UPDATE Products SET description2=?, color2=? WHERE id=?",
+    [description, color, id]
+  );
 }
